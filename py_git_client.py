e2ed424392a7b40268ad62b73ec8f046809ef06e
surreal <2002vaibhavlakhera3@gmail.com>
2024-01-10 00:21:06 +0530

    feat: create hash_object

--- py_git_client.py
+++ py_git_client.py
@@ -1,3 +1,7 @@
+import os
+import hashlib
+import zlib
+
 def init(repo):
 	os.mkdir(repo)
 	os.mkdir(os.path.join(repo, '.git'))
@@ -5,3 +9,18 @@ def init(repo):
 		os.mkdir(os.path.join(repo, '.git', name))
 	write_file(os.path.join(repo, '.git', 'HEAD'), b'ref: refs/heads/master')
 	print("Initialised empty repository: {}".format(repo))
+
+def hash_object(data, obj_type, write = True):
+	"""Compute hash of object data of given type and write to object store if "write" is true.
+	Return SHA1 object as hex string"""
+	header = '{} {}'.format(obj_type, len(data)).encode()
+	full_data = header + b'\x00' + data
+	sha1 = hashlib.sha1(full_data).hexdigest()
+	
+	if write:
+		path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+		if not os.path.exists(path):
+			os.makedirs(os.path.dirname(path), exist_ok = True)
+			write_file(path, zlib.compress(full_data))
+
+	return sha1
